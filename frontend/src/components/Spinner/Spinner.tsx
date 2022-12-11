d106fa19f2addbeb71f28ffe0446446ba69ef06b
sfgardev <sf.garol@gmail.com>
2022-12-11 15:01:20 +0800

    route to single product page

--- frontend/src/components/Spinner/Spinner.tsx
+++ frontend/src/components/Spinner/Spinner.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Spinner = () => {
   return (
     <div className="flex justify-center items-center h-full w-full">
-      <div className="relative w-24 h-24 animate-spin rounded-full bg-gradient-to-r from-purple-400 via-blue-500 to-red-400 ">
+      <div className="relative w-24 h-24 animate-spin rounded-full bg-gradient-to-r from-purple-400 to-teal-400">
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-20 h-20 bg-gray-200 rounded-full border-2 border-white"></div>
       </div>
     </div>
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import { getProducts } from "../api/getProducts";
+import Pagination from "../components/Pagination";
 import Product from "../components/Product";
 import Spinner from "../components/Spinner";
 
@@ -13,14 +13,15 @@ const Home = () => {
   return (
     <>
       {isLoading && <Spinner />}
-      <div className="grid grid-cols-2 gap-2 sm:grid-cols-3 justify-items-stretch ">
+      <div className="grid grid-cols-2 gap-2 sm:grid-cols-3">
         {!isLoading &&
           products.map((product) => (
-            <Link to="" key={product._id}>
+            <Link to={product._id} key={product._id} className="flex">
               <Product product={product} />
             </Link>
           ))}
       </div>
+      <Pagination />
     </>
   );
 };
