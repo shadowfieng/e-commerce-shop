b94658a33903f3c70cc104b988f763a53fe077c6
sfgardev <sf.garol@gmail.com>
2022-12-26 12:57:43 +0800

    added quantity of cart items to header

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { RootState } from "../../store";
 
 export const Header = () => {
+  const itemsInCart = useSelector(
+    (state: RootState) => state.cart.items.length
+  );
   return (
     <header className="fixed w-full left-0 top-0 p-4 bg-gradient-to-r from-purple-400 to-teal-400 flex items-center justify-between ">
       <Link to="/">
@@ -14,6 +19,11 @@ export const Header = () => {
         to="cart"
         className="text-[1.75rem]  hover:text-purple-500 transition"
       >
+        {itemsInCart > 0 && (
+          <span className="absolute font-bold w-4 h-4 rounded-full bg-cyan-100 text-xs text-black flex items-center justify-center">
+            {itemsInCart}
+          </span>
+        )}
         <AiOutlineShoppingCart size={32} />
       </Link>
     </header>
