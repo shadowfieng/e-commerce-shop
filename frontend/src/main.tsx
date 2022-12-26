66856978e529ce4b789766b359095a185a3e3715
sfgardev <sf.garol@gmail.com>
2022-12-26 12:40:43 +0800

    connected rtk

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-
-const queryClient = new QueryClient();
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
         <App />
-      </QueryClientProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
