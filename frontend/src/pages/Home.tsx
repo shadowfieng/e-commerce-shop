e000151888eabf414ff71cc35c9c1d30345b7def
sfgardev <sf.garol@gmail.com>
2022-12-26 12:48:54 +0800

    fetctched products with rtk query

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,19 +1,17 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { GetProductsResponse } from "../api/getProducts";
+import { useState } from "react";
+import { useGetProductsQuery } from "../api/products";
 import Pagination from "../components/Pagination";
 import Product from "../components/Product";
 import Skeleton from "../components/Skeleton";
-import { useFetch } from "../hooks/useFetch";
 
 const Home = () => {
   const [productsPerPage, setProductsPerPage] = useState(10);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const { data, loading, error, fetchData } = useFetch<GetProductsResponse>(
-    `products/?limit=${productsPerPage}&page=${currentPage}`,
-    false
-  );
+  const { data, error, isLoading, isFetching } = useGetProductsQuery({
+    page: currentPage,
+    limit: productsPerPage,
+  });
 
   const products = data?.products || [];
 
@@ -28,29 +26,21 @@ const Home = () => {
     setCurrentPage(newPage);
   };
 
-  useEffect(() => {
-    fetchData();
-  }, [productsPerPage, currentPage]);
-
   return (
     <>
-      {/* {loading && <Skeleton />} */}
-
       <Pagination
         pagination={pagination}
-        loading={loading}
+        loading={isFetching}
         onChangeItemsPerPage={changeProductsPerPage}
         onChangePage={changeProductsCurrentPage}
       />
 
       <div className="grid grid-cols-2 gap-2 sm:grid-cols-3">
-        {loading ? (
+        {isFetching ? (
           <Skeleton count={6} />
         ) : (
           products.map((product) => (
-            <Link to={product._id} key={product._id} className="flex">
-              <Product product={product} />
-            </Link>
+            <Product key={product._id} product={product} />
           ))
         )}
       </div>
